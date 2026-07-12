0cb1bf2557470d88f87053c9ac17cba1f935742b
Md-Romjan-Ali <mdromjanali224512@gmail.com>
2026-07-12 23:48:52 +0600

    added update function for story

--- src/index.ts
+++ src/index.ts
@@ -48,6 +48,18 @@ async function run() {
             const result = await successCollection.deleteOne(query)
             res.send(result)
         })
+        app.patch('/api/updatesuccess/:id', async (req, res) => {
+            const { id } = req.params
+            const query = { _id: new ObjectId(id) }
+            const update = req.body
+            const result = await successCollection.updateOne(
+                query,
+                {
+                    $set: update
+                }
+            )
+            res.send(result)
+        })
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
